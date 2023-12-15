63535fd272488e2102208abaf16c7ec086f94b66
vincent-zephyr <zvincent@nevada.unr.edu>
2023-12-15 15:22:29 -0800

    Initial Setup and Import of Assets
    Initial Build from PA-5 Solution provided by Dr. Tavakkoli

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CS-480-680-Fall-2023-Term-Project-Petersky-Vincent
+
+# SolarSystemExplorationGame
+
+# Base Code: UNR CS480 PA-5 Solution provided by Dr. Alireza Tavakkoli
+# Included classes: Camera, Engine, Window, Graphics (+ graphics_headers), Shader, Texture, 
+#                   Mesh, Object, Sphere
---
+++ SolarSystemExplorationGame/SolarSystemExplorationGame/Texture.h
@@ -0,0 +1,22 @@
+/* Texture Class Header, Author: Dr. Tavakkoli*/
+
+#pragma once
+#include "graphics_headers.h"
+#include <SOIL2/SOIL2.h>
+
+class Texture
+{
+
+public:
+	Texture();
+	Texture(const char* fileName);
+	bool loadTexture(const char* texFile);
+	GLuint getTextureID() { return m_TextureID; }
+
+private:
+	GLuint m_TextureID;
+
+	bool initializeTexture();
+
+};
+
