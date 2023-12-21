f221d7ee0a13c960b91bde6c7007f0dfa0c22928
hotcuppajojo <joannappetersky@gmail.com>
2023-12-20 21:59:21 -0800

    updated camera class with two view modes
    Signed-off-by: hotcuppajojo <joannappetersky@gmail.com>

--- SolarSystemExplorationGame/camera.cpp
+++ SolarSystemExplorationGame/camera.cpp
@@ -1,67 +1,95 @@
-/* Camera Class, Author: Dr. Tavakkoli */
-
 #include "camera.h"
 
-Camera::Camera()
-{
-
+Camera::Camera() : mode(CameraMode::Exploration) {
+    // Initialize with default values
+    cameraPos = glm::vec3(0.0f, 0.0f, 20.0f);
+    cameraFront = glm::vec3(0.0, 1.0, 1.0);
+    cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 }
 
-Camera::~Camera()
-{
-
+Camera::~Camera() {
+    // Cleanup if necessary
 }
 
-bool Camera::Initialize(int w, int h)
-{
-  //--Init the view and projection matrices
-  //  if you will be having a moving camera the view matrix will need to more dynamic
-  //  ...Like you should update it before you render more dynamic 
-  //  for this project having them static will be fine
-  //view = glm::lookAt( glm::vec3(x, y, z), //Eye Position
-  //                    glm::vec3(0.0, 0.0, 0.0), //Focus point
-  //                    glm::vec3(0.0, 1.0, 0.0)); //Positive Y is up
-  
-  view = glm::lookAt(cameraPos, cameraFront + cameraPos , cameraUp);
+bool Camera::Initialize(int w, int h) {
+    // Cache the original positions for both modes
+    originalPosExploration = cameraPos;
+    originalFrontExploration = cameraFront;
 
-  projection = glm::perspective( glm::radians(40.f), //the FoV typically 90 degrees is good which is what this is set to
-                                 float(w)/float(h), //Aspect Ratio, so Circles stay Circular
-                                 0.01f, //Distance to the near plane, normally a small value like this
-                                 100.0f); //Distance to the far plane, 
-  return true;
+    originalPosObservation = cameraPos; // Adjust this based on your initial setup
+    originalFrontObservation = cameraFront; // Adjust this based on your initial setup
+
+    UpdateProjection(w, h);
+
+    return true;
 }
 
-glm::mat4 Camera::GetProjection()
-{
-  return projection;
+glm::mat4 Camera::GetProjection() {
+    return projection;
 }
 
-glm::mat4 Camera::GetView()
-{
-  return view;
+glm::mat4 Camera::GetView() {
+    return view;
 }
 
-void Camera::updateView(glm::vec3 cameraUpdate) {
-
+void Camera::UpdateView(glm::vec3 cameraUpdate) {
     cameraFront = cameraUpdate;
     view = glm::lookAt(cameraPos, cameraFront + cameraPos, cameraUp);
 }
 
-void Camera::cameraPosVert(float speed) {
-    //cameraPos += speed * cameraUp;
+void Camera::MoveVertical(float speed) {
     cameraPos += speed * cameraFront;
     view = glm::lookAt(cameraPos, cameraFront + cameraPos, cameraUp);
 }
 
-void Camera::cameraPosHorz(float speed) {
+void Camera::MoveHorizontal(float speed) {
     cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * speed;
     view = glm::lookAt(cameraPos, cameraFront + cameraPos, cameraUp);
 }
 
-void Camera::zoom(float fov) {
-    projection = glm::perspective(glm::radians(fov), //the FoV typically 90 degrees is good which is what this is set to
-        float(800) / float(600), //Aspect Ratio, so Circles stay Circular
-        0.01f, //Distance to the near plane, normally a small value like this
-        100.0f); //Distance to the far plane, 
+void Camera::Zoom(float fov) {
+    projection = glm::perspective(glm::radians(fov), float(800) / float(600), 0.01f, 100.0f);
+}
 
+void Camera::ToggleMode() {
+    if (mode == CameraMode::Exploration) {
+        // Cache current position and front
+        originalPosExploration = cameraPos;
+        originalFrontExploration = cameraFront;
+
+        // Switch to Planetary Observation Mode
+        cameraPos = originalPosObservation;
+        cameraFront = originalFrontObservation;
+        mode = CameraMode::PlanetaryObservation;
+    }
+    else {
+        // Cache current position and front
+        originalPosObservation = cameraPos;
+        originalFrontObservation = cameraFront;
+
+        // Switch to Exploration Mode
+        cameraPos = originalPosExploration;
+        cameraFront = originalFrontExploration;
+        mode = CameraMode::Exploration;
+    }
+
+    // Update view to reflect the mode change
+    view = glm::lookAt(cameraPos, cameraFront + cameraPos, cameraUp);
+}
+
+void Camera::ResetToOriginal() {
+    if (mode == CameraMode::Exploration) {
+        cameraPos = originalPosExploration;
+        cameraFront = originalFrontExploration;
+    }
+    else {
+        cameraPos = originalPosObservation;
+        cameraFront = originalFrontObservation;
+    }
+
+    view = glm::lookAt(cameraPos, cameraFront + cameraPos, cameraUp);
+}
+
+void Camera::UpdateProjection(int w, int h) {
+    projection = glm::perspective(glm::radians(40.0f), float(w) / float(h), 0.01f, 100.0f);
 }
--- SolarSystemExplorationGame/camera.h
+++ SolarSystemExplorationGame/camera.h
@@ -1,33 +1,41 @@
-/* Camera Class Header, Author: Dr. Tavakkoli*/
-
 #ifndef CAMERA_H
 #define CAMERA_H
 
 #include "graphics_headers.h"
 
+enum class CameraMode { Exploration, PlanetaryObservation };
+
 class Camera
 {
-  public:
+public:
     Camera();
     ~Camera();
     bool Initialize(int w, int h);
     glm::mat4 GetProjection();
     glm::mat4 GetView();
 
-    void updateView(glm::vec3 cameraFront);
-    void cameraPosVert(float speed);
-    void cameraPosHorz(float speed);
-    void zoom(float fov);
-  
-  private:
-    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 20.0f);
-    glm::vec3 cameraFront = glm::vec3(0.0, 1.0, 1.0);
-    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-    double x = 0.0;
-    double y = 10.0;
-    double z = -16.0;
+    void UpdateView(glm::vec3 cameraUpdate);
+    void MoveVertical(float speed);
+    void MoveHorizontal(float speed);
+    void Zoom(float fov);
+    void ToggleMode();
+    void ResetToOriginal();
+
+private:
+    void UpdateProjection(int w, int h);
+
+    glm::vec3 cameraPos;
+    glm::vec3 cameraFront;
+    glm::vec3 cameraUp;
     glm::mat4 projection;
     glm::mat4 view;
-};
 
+    CameraMode mode;
+
+    // Caching original positions and orientations for mode switching
+    glm::vec3 originalPosExploration;
+    glm::vec3 originalFrontExploration;
+    glm::vec3 originalPosObservation;
+    glm::vec3 originalFrontObservation;
+};
 #endif /* CAMERA_H */
